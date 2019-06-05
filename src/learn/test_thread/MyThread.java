49e77e4708304fa0c2719049ca28d7cf721f1579
HUSTyankang <1227752726@qq.com>
2019-06-05 18:34:34 +0800

    修改了出现的乱码

--- src/learn/test_thread/MyThread.java
+++ src/learn/test_thread/MyThread.java
@@ -3,7 +3,7 @@ package learn.test_thread;
 public class MyThread extends Thread  {
 
 	@Override
-	public void run() {//ÖØĞ´run()
+	public void run() {
 		for (int i = 0; i < 3; i++) {
 			System.out.println(Thread.currentThread().getName() + "-->" + i);
 		}
--- src/learn/test_thread/RunableImpl.java
+++ src/learn/test_thread/RunableImpl.java
@@ -2,7 +2,7 @@ package learn.test_thread;
 
 public class RunableImpl implements Runnable {
 	@Override
-	public void run() {//实现接头的方法
+	public void run() {
 		for (int i = 0; i < 3; i++) {
 			System.out.println(Thread.currentThread().getName() + "-->" + i);
 		}
